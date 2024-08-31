1057eb0e519071bcf8535d7bdcbc92dc14321fd2
jeonghoyong <ghdyd586@gmail.com>
2024-09-01 03:17:17 +0900

    ghdyd586 3rd-commit 개인정보페이지 정보수정페이지 회원가입페이지 로그인페이지 메인페이지 완성

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Main from "./pages/Main";
 import axios from 'axios';
+import RealMain from "./pages/RealMain";
+import SignUp from "./pages/SignUp";
+import MyPage from "./pages/MyPage";
+import EditInfo from "./pages/EditInfo";
 
 
 function App() {
@@ -11,6 +15,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main/>}></Route>
+          <Route path="/pages/Main" element={<RealMain/>}></Route>
+          <Route path="/pages/SignUp" element={<SignUp/>}></Route>
+          <Route path="/pages/MyPage" element={<MyPage/>}></Route>
+          <Route path="/pages/EditInfo" element={<EditInfo/>}></Route>
+
+
+
+
         </Routes>
       </BrowserRouter>
   );

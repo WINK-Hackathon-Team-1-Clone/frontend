f3eb2e8eed2ef2012f331c0ec3221000f9d02896
cg10036 <cg10036@gmail.com>
2024-09-01 02:04:47 +0900

    add: map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import  { React, useEffect, useState } from "react";
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { React, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
-import axios from 'axios';
-
+import axios from "axios";
+import Map from "./pages/Map";
 
 function App() {
   return (
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main/>}></Route>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/map" element={<Map />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

33800b595becd05b4daae10d51835c9ebc17f261
cg10036 <cg10036@gmail.com>
2024-09-01 09:58:46 +0900

    fix: change name

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -16,7 +16,7 @@ function Main() {
             borderRadius: "10px",
           }}
         >
-          <h1 style={{ marginTop: "60px" }}>서비스 이름</h1>
+          <h1 style={{ marginTop: "60px" }}>첫걸음</h1>
           <Row justify="center">
             <Col xs={24} sm={16} md={12} lg={8} xl={6}>
               <Form
